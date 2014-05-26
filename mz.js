7957ee564ed8723facb69d229ea3d42ff813bc8d
Zach <zackify@gmail.com>
2014-05-26 14:14:09 -0700

    changed to insertRule instead of addRule

--- mz.js
+++ mz.js
@@ -1,33 +1,33 @@
 function mz(selector){
-	var self = {}
-	self.selector = selector
+	var self = {};
+	self.selector = selector;
 	self.element = document.querySelector(self.selector);
 
 	//grab the html of an element mz('selector').html()
 	self.html = function(){
-		return self.element
+		return self.element;
 	}
-	
-	//add a CSS rule to a stylesheet easily. mz('selector').addRule(name,value)
-	self.addRule = function(name,value,stylesheet){
-		if(!stylesheet) stylesheet = 0
 
+	//add a CSS rule to a stylesheet easily. mz('selector').insertRule(name,value)
+	self.insertRule = function(name,value,position,stylesheet){
+		if(!stylesheet) stylesheet = 0;
+		if(!position) position = 0;
 		//check for the specified style sheet
 		if(document.styleSheets[stylesheet]){
-			document.styleSheets[stylesheet].addRule(self.selector,name + ': ' + value);
+			document.styleSheets[stylesheet].insertRule(self.selector + '{'+ name + ': ' + value + '}',position);
 		}
 
 		//if that one doesn't exist, check if there are any stylesheets at all
 		else if(document.styleSheets[0]){
 			//warn them
-			console.log("the specified stylesheet doesn't exist, the first was used instead")
-			document.styleSheets[0].addRule(self.selector,name + ': ' + value);
+			console.warn("the specified stylesheet doesn't exist, the first was used instead");
+			document.styleSheets[0].insertRule(self.selector + '{'+ name + ': ' + value + '}',position);
 		}
 		//if none are available, give error
-		else console.log('No style sheets available to modify')
+		else console.error('No style sheets available to modify');
 
-		return self
+		return self;
 	}
 
-	return self
+	return self;
 }
